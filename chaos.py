7c3e11bf5545d6e0a0e06c6c4d99f4311631d11b
Dallas Brooks <dallasbrooks12@gmail.com>
2023-02-22 22:04:00 -0800

    init file

---
+++ chaos.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import random
+
+DEF_N = 100
+DEF_M = 5000
+
+def main(n, m):
+	targets = ((0, 0), (n/2, n), (n, 0))
+	count = len(targets)
+	plt.figure()
+	last = (targets[0])
+	for i in range(count):
+		t = targets[random.randrange(count)]
+		x = (last[0] + t[0]) / 2.0
+		y = (last[1] + t[1]) / 2.0
+		plt.plot(x, y, marker='.', markersize=1)
+		last = (x, y)
+	plt.show()
+
+if __name__ == '__main__':
+	main(DEF_N, DEF_M)
